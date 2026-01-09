68d7b1f67ad0894f1b83194e021ecce9b1c88750
Jervin Villones <jervinvillones6@gmail.com>
2026-01-09 12:35:23 +0800

    Updated style.css

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const darkToggle = document.querySelector(".darkmode");
 const bodydark = document.querySelector(".darkmode-body");
 
 darkToggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  bodydark.classList.toggle("active");
+    document.body.classList.toggle("dark-mode");
+    bodydark.classList.toggle("active");
 });
