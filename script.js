9f7faedfc69d248d4c7fa080fc5d7bdcb4635eb7
Jervin Villones <jervinvillones6@gmail.com>
2026-01-09 20:03:57 +0800

    Updated style.css

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ darkToggle.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
     bodydark.classList.toggle("active");
 });
+
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".preloader");
+
+    setTimeout(() => {
+      loader.classList.add("hide");
+    }, 3200); 
+});
