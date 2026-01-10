6f1b070f89a9ca03f62d5f880d58d397bda39748
Jervin Villones <jervinvillones6@gmail.com>
2026-01-10 13:50:43 +0800

    Updated style.css

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const darkToggle = document.querySelector(".darkmode");
+const lightToggle = document.querySelector(".lightmode");
 const bodydark = document.querySelector(".darkmode-body");
+const mode = document.querySelector(".modes");
+const modal = document.querySelector(".modal");
+
+mode.addEventListener("click", () => {
+    modal.classList.toggle("show");
+});
+
+lightToggle.addEventListener("click", () => {
+    document.body.classList.remove("dark-mode");
+    bodydark.classList.remove("active");
+});
 
 darkToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    bodydark.classList.toggle("active");
+    document.body.classList.add("dark-mode");
+    bodydark.classList.add("active");
 });
 
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".preloader");
 
-    setTimeout(() => {
-      loader.classList.add("hide");
-    }, 3200); 
-});
+
+//window.addEventListener("load", () => {
+    //const loader = document.querySelector(".preloader");
+
+   // setTimeout(() => {
+    //  loader.classList.add("hide");
+    //}, 3200); 
+//});
+
